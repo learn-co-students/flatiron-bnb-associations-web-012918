5bb2f8e380c41677b6a56e9c3285320cd7892170
izzee <ichabon@me.com>
2018-02-16 11:20:49 -0500

    Done.

---
+++ db/migrate/02_create_listings.rb
@@ -0,0 +1,13 @@
+class CreateListings < ActiveRecord::Migration
+  def change
+    create_table :listings do |t|
+      t.integer :host_id
+      t.integer :neighborhood_id
+      t.string :address
+      t.string :listing_type
+      t.string :title
+      t.string :description
+      t.float :price
+    end
+  end
+end
