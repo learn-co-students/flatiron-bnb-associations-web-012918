8ee7dea118073584879f3a2c6df594fc939f0a4e
aalexander3 <aralx73@gmail.com>
2018-02-16 10:59:27 -0500

    Done.

---
+++ bin/run.rb
@@ -0,0 +1,12 @@
+require "./config/environments/development"
+require "./config/environments/production"
+require "./config/environments/test"
+require "pry"
+
+
+
+binding.pry
+
+
+
+"hi"
---
+++ db/migrate/007_update_date_data_type.rb
@@ -0,0 +1,6 @@
+class UpdateDateDataType < ActiveRecord::Migration
+  def change
+    change_column(:reservations, :checkin, :date)
+    change_column(:reservations, :checkout, :date)
+  end
+end
